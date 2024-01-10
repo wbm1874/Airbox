62c7f983e43f23786fd12613579aae7fb2b62955
Ewan Cunningham <ewan.cunningham@careinspectorate.gov.scot>
2024-01-10 08:31:50 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Airbox
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
